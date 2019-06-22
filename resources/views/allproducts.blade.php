806e5e7537b189864c747f7274a9ef3a36519b9b
David Basil <info@basildavid.com>
2019-06-22 14:15:44 +0400

    add product image to storage

--- resources/views/allproducts.blade.php
+++ resources/views/allproducts.blade.php
@@ -159,17 +159,21 @@
                 <div class="product-image-wrapper">
                   <div class="single-products">
                     <div class="productinfo text-center">
-                      <img src="" alt="" />
+                      <img src="{{ Storage::disk('local')->url('product_images/'.$product->image) }}" alt="" />
                       <h2>{{$product->price}}</h2>
                       <p>{{ $product->name }}</p>
                       <p>{{$product->description}}</p>
-                      <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                      <a href="#" class="btn btn-default add-to-cart">
+                        <i class="fa fa-shopping-cart"></i>Add to cart
+                      </a>
                     </div>
                     <div class="product-overlay">
                       <div class="overlay-content">
                         <h2>{{$product->price}}</h2>
                         <p>{{ $product->name }}</p>
-                        <a href="" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                        <a href="" class="btn btn-default add-to-cart">
+                          <i class="fa fa-shopping-cart"></i>Add to cart
+                        </a>
                       </div>
                     </div>
                   </div>
