c652b2bb31086e6a42c5720921e3daa81e417c79
David Basil <info@basildavid.com>
2019-06-24 17:49:54 +0400

    add editProductFormImage.blade

---
+++ resources/views/admin/editProductImageForm.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.admin')
+
+@section('body')
+
+  @if($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        <li>{{ $errors->all() }}</li>
+      </ul>
+    </div>
+  @endif
+
+  <h3>Current Image</h3>
+  <div>
+    <img src="{{ asset('storage') }}/{{$product['image']}}" alt="">
+  </div>
+
+  <form 
+    action="/admin/updateImage/{{ $product->id }}" 
+    method="post" 
+    enctype="multipart/form-data">
+    @csrf
+
+    <div class="form-group">
+      <label for="description">Update Image</label>
+      <input 
+        type="file" 
+        name="image" 
+        placeholder="image" 
+        value="{{ $product->image }}"
+        required>
+    </div>
+
+    <button type="submit" name="submit" class="btn btn-default">Submit</button>
+  </form>
+
+@endsection
